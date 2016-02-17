a141a2d7dd0b646205311db983bd293a2d3c2b63
Pedro Henrique Altoé <phaltoe@users.noreply.github.com>
2016-02-17 15:54:24 -0200

    Update truck.rb

--- lib/truck.rb
+++ lib/truck.rb
@@ -16,16 +16,3 @@ class Truck
     @items.delete(box)
   end
 end
-
-
-
-
-
-
-
-
-  # We're going to need some trucks. Make a `Truck` class that specifies how many boxes
-  # it can hold (and a `Box` class that stores the name of its owner, which should be
-  # an `Occupant` object) and stores these `Box` objects in an array. Make sure the `Truck`
-  # has a method to check if it is `full?`, one to `add_box`, and one to `remove_box`.
-  # Also give the `Truck` a method to unload all the boxes of a particular `Occupant` at once.
